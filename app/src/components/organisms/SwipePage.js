0d6900f7f400419fc678deb72052abb1e4136a24
lamolivi <olivia.lam@alumni.ubc.ca>
2022-01-15 15:55:22 -0800

    swiping page

---
+++ app/src/components/organisms/SwipePage.js
@@ -0,0 +1,139 @@
+import React, {useEffect, useState} from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image, 
+} from 'react-native';
+
+import SwipePageHeader from '../molecules/SwipePageHeader';
+// TODO : need to document dependencies for this
+import TinderCard from 'react-tinder-card'
+
+import NopeStamp from '../atoms/NopeStamp';
+import LikeStamp from '../atoms/LikeStamp';
+
+const db = [
+  {
+   id: 1,
+   url: 'https://picsum.photos/id/0/200/300',
+   desc: 'Macbook Air, Priced at: $200'
+  },
+ 
+  {
+   id: 2,
+   url: 'https://picsum.photos/id/157/200/300',
+   desc: 'Men\'s Accessory Kit, Priced at: $75',
+  },
+ 
+  {
+ 
+   id: 3,
+   url: 'https://picsum.photos/id/250/200/300',
+   desc: 'Polaroid Camera Year 1999, Priced at $118'
+  },
+ 
+  {
+   id: 4,
+   url: 'https://picsum.photos/id/26/200/300',
+   desc: 'Pennyboard (barely used), Priced at $60'
+  },
+
+  {
+    id: 5,
+    url: 'https://picsum.photos/id/370/200/300',
+    desc: 'Macbook Pro, Priced at $500'
+  }
+ 
+ ];
+
+function SwipePage({}){
+
+  const cards = db;
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete)
+    setLastDirection(direction);
+  }
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!')
+  }
+
+	return (
+		<SafeAreaView style={{backgroundColor: '#155A81'}}>
+			<SwipePageHeader></SwipePageHeader>
+
+      <View style={styles.cardContainer}>
+        <Text style={{zIndex: 0, bottom: -200, marginLeft: 30, fontSize: 15}}>No more items available right now!</Text>
+
+        <View style={{flex: 1, flexDirection: 'row'}}>
+        {/* <NopeStamp></NopeStamp>
+        <LikeStamp></LikeStamp> */}
+        </View>
+
+     {cards.map((card) => 
+
+     
+     <TinderCard key={card.id} onSwipe={(dir) => swiped(dir, card.id)} onCardLeftScreen={() => outOfFrame(card.id)}>
+       <View style={styles.card}>
+       <Image style={styles.image} source={{uri: card.url}} name={card.id}></Image>
+       <Text style={styles.cardTitle}>{card.desc.toUpperCase()}</Text>
+       </View>
+     </TinderCard>
+     
+      
+     )
+     }
+
+    </View>
+
+    {/* <Footer></Footer> */}
+      
+		</SafeAreaView>
+	);
+}
+
+const styles = StyleSheet.create({
+
+  cardContainer: {
+    marginLeft: 40,
+    marginTop: 35,
+  },
+
+  card: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    maxWidth: 260,
+    height: 300,
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+    shadowRadius: 20,
+    borderRadius: 20,
+    resizeMode: 'cover',
+    
+  },
+
+  cardTitle: {
+    position: 'absolute',
+    bottom: -150,
+    margin: 15,
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+
+  image: {
+    height: 450,
+    width: 300,
+    borderRadius: 20,
+    overflow: 'hidden'
+  }
+
+});
+
+
+export default SwipePage;
