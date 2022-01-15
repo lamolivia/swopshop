6b678a34a520809a8dc427c0d74dbf0054cbc4bf
FrancisGodinho <Francisgodinho2010@hotmail.com>
2022-01-15 12:49:13 -0800

    started main page

--- app/src/components/organisms/Main.js
+++ app/src/components/organisms/Main.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 function Main({}){
 	return (
 		<SafeAreaView>
+			<Image style={{}} source={{uri: "../../../assets/swoplogo.PNG"}}/>
 			<Text>HomePage</Text>
 		</SafeAreaView>
 	);
