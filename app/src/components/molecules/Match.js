493c9af60fff9be8836ae1cdbc7d81d3f2a1c8f7
lamolivi <olivia.lam@alumni.ubc.ca>
2022-01-16 02:37:42 -0800

    splash png and match.js

---
+++ app/src/components/molecules/Match.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+import {
+Image,
+  Text,
+  View,
+  Dimensions,
+  Modal,
+  TouchableWithoutFeedback
+} from 'react-native';
+
+const deviceHeight = Dimensions.get('window').height
+export class Match extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            show: false
+        }
+    }
+
+    show = () => {
+        this.setState({show:true})
+    }
+
+    close = () => {
+        this.setState({show: false})
+    }
+
+    renderOutsideTouchable(onTouch) {
+        const view = <View style={{flex: 1, width: '100%'}}/>
+        if (!onTouch) return view
+
+        return (
+            <TouchableWithoutFeedback onPress={onTouch} style={{flex:1, width: '100%'}}>
+            {view}
+            </TouchableWithoutFeedback>
+            )
+    }
+
+    render() {
+        let {show} = this.state
+        const {onTouchOutside, matchDegree} = this.props
+        
+        return (
+            <Modal
+                animationType={'fade'}
+                transparent={true}
+                visible={show}
+                onRequestClose={this.close}
+            >
+
+            <View style={{flex: 1, backgroundColor: 'white', justifyContent: 'flex-end'}}>
+                
+                {this.renderOutsideTouchable(onTouchOutside)}
+                <View style={{backgroundColor: 'white', 
+                            width: '100%',
+                            borderTopRightRadius: 10,
+                            borderTopLeftRadius: 10,
+                            paddingHorizontal: 10,
+                            maxHeight: deviceHeight * 0.3,
+                            bottom: 300
+                            }}>
+
+                <View style={{height: '50%', backgroundColor: 'white', justifyContent: 'center', alignItems: 'center'}}>
+
+                    <Image style={{height: 200, width: 300}} source={require('../../../assets/match_splash.png')}/> 
+                    <Text> You have a {matchDegree}-way trade</Text>
+                </View>
+
+                </View>
+                
+            </View>
+            
+            </Modal>
+
+              
+            
+            )
+    }
+
+
+
+}
+
+export default Match
