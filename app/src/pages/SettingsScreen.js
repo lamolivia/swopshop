63a3d2d2c4bfb629c910db9d9c84be9c3b5188dc
Mason Wong <masonwong19@gmail.com>
2022-01-16 10:43:37 -0800

    Adding logout button

--- app/src/pages/SettingsScreen.js
+++ app/src/pages/SettingsScreen.js
@@ -1,20 +1,29 @@
 import { signOut } from "firebase/auth";
 import React from "react";
-import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import { auth } from "../utils/firebase";
+import SwopButton from "../components/atoms/SwopButton";
+import headers from "../styles/headers";
+import { useGlobalContext } from "../utils/context";
 
 const SettingScreen = ({ navigation }) => {
+  const { curUser } = useGlobalContext();
+  
   const logout = () => {
     signOut(auth).then(navigation.navigate("Login"));
   };
   return (
-    <SafeAreaView>
-      <Text>Hello Settings</Text>
-      <TouchableOpacity onPress={logout}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <Text style={headers.h1}>
+        @{curUser.username}
+      </Text>
+      <SwopButton title={'Logout'} onPress={logout}/>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { marginHorizontal: 10 }
+})
 
 export default SettingScreen;
